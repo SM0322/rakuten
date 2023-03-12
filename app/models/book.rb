d75363139eb15d6cb6308991ee9c4fa0fd5216d9
SM0322 <118785066+SM0322@users.noreply.github.com>
2023-03-12 23:55:52 +0000

    アソシエーションとルーティング完了

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,5 @@
 class Book < ApplicationRecord
+  self.primary_key = "isbn"
+  has_many :reviews, dependent: :destroy
+  has_many :bookcases, dependent: :destroy
 end
---
+++ config/initializers/rakuten.rb
@@ -0,0 +1,3 @@
+RakutenWebService.configure do |c|
+    c.application_id = '1083916622969599350'
+end
