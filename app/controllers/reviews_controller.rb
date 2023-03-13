0fcd0a7d22a6eb475951b90debefa3f5a1ddd592
SM0322 <118785066+SM0322@users.noreply.github.com>
2023-03-13 00:04:24 +0000

    登録した本にコメント可能

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,18 @@
+class ReviewsController < ApplicationController
+  def create
+    @book = Book.find(params[:book_id])
+    @review = current_user.reviews.new(reviews_params)
+    @review.book_id = @book.id
+    if @review.save!
+      redirect_to book_path(@book)
+    else
+      redirect_to books_path
+    end 
+  end 
+  
+  private
+  
+  def reviews_params
+    params.require(:review).permit(:impression)
+  end
+end
